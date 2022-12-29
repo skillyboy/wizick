3434cc9b52ca2287beeb0c6ff0cbc85340927d41
Skillyboy <fakoredeabbas@gmail.com>
2022-12-29 03:57:28 +0100

    New changes

--- dms/serializers.py
+++ dms/serializers.py
@@ -1,13 +1,13 @@
-from rest_framework import serializers
-from . models import *
+# from rest_framework import serializers
+# from . models import *
   
-class EsignSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Esign
-        fields = ['id','user', 'signature']
+# class EsignSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Esign
+#         fields = ['id','user', 'signature']
 
-class ProfileDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProfileDetail
-        fields = ['id','username', 'company','first_name','last_name', 'url','email', 'phone','date_joined','p_img']
+# class ProfileDetailSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ProfileDetail
+#         fields = ['id','username', 'company','first_name','last_name', 'url','email', 'phone','date_joined','p_img']
 
